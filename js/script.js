58a8f0ba682da59c9e0719e3a6edb702c887e903
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-05 13:54:07 +0100

    [add] array di oggetti

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const team = [
+{
+  nome: "Wayne",
+  cognome: "Barnett",
+  role: "Founder & CEO",			
+  img: "wayne-barnett-founder-ceo.jpg",
+},
+{
+  nome: "Angela ",
+  cognome: "Caroll",
+  role: "Chief Editor",			
+  img: "angela-caroll-chief-editor.jpg",
+},
+{
+  nome: "Walter",
+  cognome: "Gordon	",
+  role: "Office Manager",			
+  img: "walter-gordon-office-manager.jpg",
+},
+{
+  nome: "Angela",
+  cognome: "Lopez",
+  role: "Social Media Manager",			
+  img: "angela-lopez-social-media-manager.jpg",
+},
+{
+  nome: "Scott",
+  cognome: "Estrada",
+  role: "Developer",			
+  img: "scott-estrada-developer.jpg",
+},
+{
+  nome: "Barbara",
+  cognome: "Ramos",
+  role: "Graphic Designer",			
+  img: "barbara-ramos-graphic-designer.jpg",
+}
+]
+
